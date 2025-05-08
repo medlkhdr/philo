a6908367c0b986ee1437f42b34dc91d955fec03f
feeedback <m0h4m3dl4khd4r@gmail.com>
2025-05-08 16:18:56 +0100

    OWNEDBYFEEDBACK

--- init.c
+++ init.c
@@ -1,256 +1,274 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: feedback <feedback@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/08 16:18:40 by feedback          #+#    #+#             */
+/*   Updated: 2025/05/08 16:18:40 by feedback         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philos.h"
 
-unsigned long time_now_ms()
+unsigned long	time_now_ms(void)
 {
-  struct timeval tv;
-  gettimeofday(&tv, NULL);
-  return (tv.tv_sec * 1000 + tv.tv_usec / 1000);
+	struct timeval	tv;
+
+	gettimeofday(&tv, NULL);
+	return (tv.tv_sec * 1000 + tv.tv_usec / 1000);
 }
-void ft_msleep(unsigned long msec)
+void	ft_msleep(unsigned long msec)
 {
-  size_t start;
-  size_t target;
+	size_t	start;
+	size_t	target;
 
-  start = time_now_ms();
-  target = start + msec;
-  while (time_now_ms() < target)
-      usleep(100);
-}
-
-void print_status(t_ph *ph, char *status)
-{
-  unsigned long time;
-
-  time = time_now_ms() - ph->data->start_time;
-  pthread_mutex_lock(ph->mutex.stop_mutex);
-  if (!ph->data->stop)
-  {
-    pthread_mutex_lock(ph->mutex.print_mutex);
-    printf("%lu %d %s\n", time, ph->id, status);
-    pthread_mutex_unlock(ph->mutex.print_mutex);
-  }
-  pthread_mutex_unlock(ph->mutex.stop_mutex);
-}
-void eat(t_ph *ph, int *meal_counter)
-{
-
-  if(ph->data->notme != -1)
-  {
-    if(*meal_counter == ph->data->notme)
-    return;
-    (*meal_counter)++;
-  }
-  if(ph->id % 2 == 0)
-  {
-    pthread_mutex_lock(ph->mutex.lfork);
-    pthread_mutex_lock(ph->mutex.rfork);
-  }
-  else
-  {
-    pthread_mutex_lock(ph->mutex.rfork);
-    pthread_mutex_lock(ph->mutex.lfork);
-  }
-  print_status(ph, FORK);
-  print_status(ph, FORK);
-  ft_msleep(ph->data->tte);
-  print_status(ph, EAT);
-  pthread_mutex_lock(ph->mutex.mutex_last_meal);
-  ph->last_meal = time_now_ms();
-  pthread_mutex_unlock(ph->mutex.mutex_last_meal);
-  if(ph->id % 2 == 0)
-  {
-    pthread_mutex_unlock(ph->mutex.rfork);
-    pthread_mutex_unlock(ph->mutex.lfork);
-  }
-  else
-  {
-    pthread_mutex_unlock(ph->mutex.lfork);
-    pthread_mutex_unlock(ph->mutex.rfork);
-  }
+	start = time_now_ms();
+	target = start + msec;
+	while (time_now_ms() < target)
+		usleep(100);
 }
 
-
-void issleep(t_ph *ph)
+void	print_status(t_ph *ph, char *status)
 {
-  ft_msleep(ph->data->tts);
-  print_status(ph, SLEEP);
+	unsigned long	time;
+
+	time = time_now_ms() - ph->data->start_time;
+	pthread_mutex_lock(ph->mutex.stop_mutex);
+	if (!ph->data->stop)
+	{
+		pthread_mutex_lock(ph->mutex.print_mutex);
+		printf("%lu %d %s\n", time, ph->id, status);
+		pthread_mutex_unlock(ph->mutex.print_mutex);
+	}
+	pthread_mutex_unlock(ph->mutex.stop_mutex);
+}
+void	eat(t_ph *ph, int *meal_counter)
+{
+	if (ph->data->notme != -1)
+	{
+		if (*meal_counter == ph->data->notme)
+			return ;
+		(*meal_counter)++;
+	}
+	if (ph->id % 2 == 0)
+	{
+		pthread_mutex_lock(ph->mutex.lfork);
+		pthread_mutex_lock(ph->mutex.rfork);
+	}
+	else
+	{
+		pthread_mutex_lock(ph->mutex.rfork);
+		pthread_mutex_lock(ph->mutex.lfork);
+	}
+	print_status(ph, FORK);
+	print_status(ph, FORK);
+	ft_msleep(ph->data->tte);
+	print_status(ph, EAT);
+	pthread_mutex_lock(ph->mutex.mutex_last_meal);
+	ph->last_meal = time_now_ms();
+	pthread_mutex_unlock(ph->mutex.mutex_last_meal);
+	if (ph->id % 2 == 0)
+	{
+		pthread_mutex_unlock(ph->mutex.rfork);
+		pthread_mutex_unlock(ph->mutex.lfork);
+	}
+	else
+	{
+		pthread_mutex_unlock(ph->mutex.lfork);
+		pthread_mutex_unlock(ph->mutex.rfork);
+	}
 }
 
-void think(t_ph *ph)
+void	issleep(t_ph *ph)
 {
-  print_status(ph, THINK);
-}
-void die(t_ph *ph)
-{
-  ft_msleep(ph->data->ttd);
-  print_status(ph, DEATH);
-}
-void *routine(void *arg)
-{
-  t_ph *ph = (t_ph *)arg;
-  int meal_counter = 0;
-
-  while (1)
-  {
-    if(ph->data->nop == 1)
-    {
-      print_status(ph, FORK);
-      die(ph);
-      return NULL;
-    }
-    pthread_mutex_lock(ph->mutex.stop_mutex);
-    if (ph->data->stop)
-    {
-      pthread_mutex_unlock(ph->mutex.stop_mutex);
-      return NULL;
-    }
-    pthread_mutex_unlock(ph->mutex.stop_mutex);
-    if (ph->data->notme != -1 && meal_counter == ph->data->notme)
-    {
-      ph->data->stop = true;
-      return NULL;
-    }
-    eat(ph, &meal_counter);
-    issleep(ph);
-    think(ph);
-  }
-  return NULL;
-}
-void *monitor_routine(void *arg)
-{
-  t_ph *ph;
-  int size;
-  int i;
-
-  ph = (t_ph *)arg;
-  size = ph->data->nop;
-  while (1)
-  {
-    i = -1;
-    while (++i < size)
-    {
-      if(ph[i].data->stop)
-        return NULL;
-      pthread_mutex_lock(ph[i].mutex.mutex_last_meal);
-      if (ph[i].last_meal != 0 && time_now_ms() - ph[i].last_meal > ph[i].data->ttd)
-      {
-        pthread_mutex_unlock(ph[i].mutex.mutex_last_meal);
-        die(&ph[i]);
-        pthread_mutex_lock(ph[i].mutex.stop_mutex);
-        ph[i].data->stop = true;
-        pthread_mutex_unlock(ph[i].mutex.stop_mutex);
-        return NULL;
-      }
-      pthread_mutex_unlock(ph[i].mutex.mutex_last_meal);
-    }
-  }
-  return NULL;
+	ft_msleep(ph->data->tts);
+	print_status(ph, SLEEP);
 }
 
-void Housekeeping(t_housekeeped clean)
+void	think(t_ph *ph)
 {
-  int size;
-  int i;
+	print_status(ph, THINK);
+}
+void	die(t_ph *ph)
+{
+	ft_msleep(ph->data->ttd);
+	print_status(ph, DEATH);
+}
+void	*routine(void *arg)
+{
+	t_ph	*ph;
+	int		meal_counter;
 
-  i = 0;
-  size = clean.ph[0].data->nop;
+	ph = (t_ph *)arg;
+	meal_counter = 0;
+	while (1)
+	{
+		if (ph->data->nop == 1)
+		{
+			print_status(ph, FORK);
+			die(ph);
+			return (NULL);
+		}
+		pthread_mutex_lock(ph->mutex.stop_mutex);
+		if (ph->data->stop)
+		{
+			pthread_mutex_unlock(ph->mutex.stop_mutex);
+			return (NULL);
+		}
+		pthread_mutex_unlock(ph->mutex.stop_mutex);
+		if (ph->data->notme != -1 && meal_counter == ph->data->notme)
+		{
+			ph->data->stop = true;
+			return (NULL);
+		}
+		eat(ph, &meal_counter);
+		issleep(ph);
+		think(ph);
+	}
+	return (NULL);
+}
+void	*monitor_routine(void *arg)
+{
+	t_ph	*ph;
+	int		size;
+	int		i;
 
-  while (i < size)
-  {
-    pthread_mutex_destroy(&clean.forks[i]);
-    i++;
-  }
-  pthread_mutex_destroy(clean.mutex_last_meal);
-  pthread_mutex_destroy(clean.stop);
-  free(clean.ph);
-  free(clean.thread);
-  free(clean.mutex_last_meal);
-  free(clean.forks);
-  free(clean.print_mutex);
-  free(clean.stop);
+	ph = (t_ph *)arg;
+	size = ph->data->nop;
+	while (1)
+	{
+		i = -1;
+		while (++i < size)
+		{
+			if (ph[i].data->stop)
+				return (NULL);
+			pthread_mutex_lock(ph[i].mutex.mutex_last_meal);
+			if (ph[i].last_meal != 0 && time_now_ms()
+				- ph[i].last_meal > ph[i].data->ttd)
+			{
+				pthread_mutex_unlock(ph[i].mutex.mutex_last_meal);
+				die(&ph[i]);
+				pthread_mutex_lock(ph[i].mutex.stop_mutex);
+				ph[i].data->stop = true;
+				pthread_mutex_unlock(ph[i].mutex.stop_mutex);
+				return (NULL);
+			}
+			pthread_mutex_unlock(ph[i].mutex.mutex_last_meal);
+		}
+	}
+	return (NULL);
 }
 
-void creater_joiner(t_ph *ph , pthread_t *thread)
+void	Housekeeping(t_housekeeped clean)
 {
-  int size;
-  t_data *data;
-  pthread_t  monitor;
-  int i;
+	int	size;
+	int	i;
 
-  i = 0;
-  data = ph[0].data;
-  size = ph[0].data->nop;
-  while(i < size)
-  {
-    pthread_create(&thread[i], NULL, &routine, (void *)&ph[i]);
-    i++;
-  }
-  if(data->nop != 1)
-    pthread_create(&monitor, NULL, &monitor_routine, (void *)ph);
-  i = 0;
-  while (i < size)
-   {
-     pthread_join(thread[i], NULL);
-     i++;
-   }
- if(size != 1)
-  pthread_join(monitor, NULL);
-}
-void init_used_data(t_ph *ph, t_data *data, pthread_t *thread, pthread_mutex_t *forks )
-{
-  int size;
-  int i;
-  pthread_mutex_t *mutex_last_meal;
-  pthread_mutex_t *stop;
-  pthread_mutex_t *print_mutex;
-  print_mutex = malloc(sizeof(pthread_mutex_t));
-  mutex_last_meal = malloc(sizeof(pthread_mutex_t));
-  pthread_mutex_init(print_mutex , NULL);
-  stop = malloc(sizeof(pthread_mutex_t));
-  pthread_mutex_init(stop, NULL);
-  pthread_mutex_init(mutex_last_meal, NULL);
-  data->stop = false;
-  size = data->nop;
-  i = 0;
-  while (i < size)
-  {
-    pthread_mutex_init(&forks[i], NULL);
-    ph[i].mutex.rfork = &forks[i];
-    ph[i].mutex.lfork = &forks[(i + 1) % size];
-    ph[i].mutex.stop_mutex = stop;
-    ph[i].mutex.print_mutex = print_mutex;
-    ph[i].mutex.mutex_last_meal = mutex_last_meal;
-    ph[i].data = data;
-    ph[i].id = i + 1;
-    ph[i++].last_meal = 0;
-  }
+	i = 0;
+	size = clean.ph[0].data->nop;
+	while (i < size)
+	{
+		pthread_mutex_destroy(&clean.forks[i]);
+		i++;
+	}
+	pthread_mutex_destroy(clean.mutex_last_meal);
+	pthread_mutex_destroy(clean.stop);
+	free(clean.ph);
+	free(clean.thread);
+	free(clean.mutex_last_meal);
+	free(clean.forks);
+	free(clean.print_mutex);
+	free(clean.stop);
 }
 
-t_housekeeped cleaner_init(t_ph *ph , pthread_t *thread , pthread_mutex_t *forks)
+void	creater_joiner(t_ph *ph, pthread_t *thread)
 {
-  static t_housekeeped clean;
-  clean.forks = forks;
-  clean.mutex_last_meal = ph[0].mutex.mutex_last_meal;
-  clean.ph = ph;
-  clean.print_mutex = ph[0].mutex.print_mutex;
-  clean.stop = ph[0].mutex.stop_mutex;
-  clean.thread = thread;
-  return clean;
-}
-void init(t_data *data)
-{
-  int size;
-  t_housekeeped clean;
-  pthread_mutex_t *forks;
-  pthread_t *thread;
+	int			size;
+	t_data		*data;
+	pthread_t	monitor;
+	int			i;
 
-  size = data->nop;
-  forks = malloc(sizeof(pthread_mutex_t) * size);
-  thread = malloc(sizeof(pthread_t) * size);
-  t_ph *ph = malloc(sizeof(t_ph) * size);
-  init_used_data(ph, data , thread , forks);
-  clean = cleaner_init(ph, thread, forks);
-  data->start_time = time_now_ms();
-  creater_joiner(ph, thread);
-  Housekeeping(clean);
+	i = 0;
+	data = ph[0].data;
+	size = ph[0].data->nop;
+	while (i < size)
+	{
+		pthread_create(&thread[i], NULL, &routine, (void *)&ph[i]);
+		i++;
+	}
+	if (data->nop != 1)
+		pthread_create(&monitor, NULL, &monitor_routine, (void *)ph);
+	i = 0;
+	while (i < size)
+	{
+		pthread_join(thread[i], NULL);
+		i++;
+	}
+	if (size != 1)
+		pthread_join(monitor, NULL);
+}
+void	init_used_data(t_ph *ph, t_data *data, pthread_t *thread,
+		pthread_mutex_t *forks)
+{
+	int				size;
+	int				i;
+	pthread_mutex_t	*mutex_last_meal;
+	pthread_mutex_t	*stop;
+	pthread_mutex_t	*print_mutex;
+
+	print_mutex = malloc(sizeof(pthread_mutex_t));
+	mutex_last_meal = malloc(sizeof(pthread_mutex_t));
+	pthread_mutex_init(print_mutex, NULL);
+	stop = malloc(sizeof(pthread_mutex_t));
+	pthread_mutex_init(stop, NULL);
+	pthread_mutex_init(mutex_last_meal, NULL);
+	data->stop = false;
+	size = data->nop;
+	i = 0;
+	while (i < size)
+	{
+		pthread_mutex_init(&forks[i], NULL);
+		ph[i].mutex.rfork = &forks[i];
+		ph[i].mutex.lfork = &forks[(i + 1) % size];
+		ph[i].mutex.stop_mutex = stop;
+		ph[i].mutex.print_mutex = print_mutex;
+		ph[i].mutex.mutex_last_meal = mutex_last_meal;
+		ph[i].data = data;
+		ph[i].id = i + 1;
+		ph[i++].last_meal = 0;
+	}
+}
+
+t_housekeeped	cleaner_init(t_ph *ph, pthread_t *thread,
+		pthread_mutex_t *forks)
+{
+	static t_housekeeped	clean;
+
+	clean.forks = forks;
+	clean.mutex_last_meal = ph[0].mutex.mutex_last_meal;
+	clean.ph = ph;
+	clean.print_mutex = ph[0].mutex.print_mutex;
+	clean.stop = ph[0].mutex.stop_mutex;
+	clean.thread = thread;
+	return (clean);
+}
+void	init(t_data *data)
+{
+	int				size;
+	t_housekeeped	clean;
+	pthread_mutex_t	*forks;
+	pthread_t		*thread;
+	t_ph			*ph;
+
+	size = data->nop;
+	forks = malloc(sizeof(pthread_mutex_t) * size);
+	thread = malloc(sizeof(pthread_t) * size);
+	ph = malloc(sizeof(t_ph) * size);
+	init_used_data(ph, data, thread, forks);
+	clean = cleaner_init(ph, thread, forks);
+	data->start_time = time_now_ms();
+	creater_joiner(ph, thread);
+	Housekeeping(clean);
 }
--- philos.c
+++ philos.c
@@ -1,8 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philos.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: feedback <feedback@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/08 16:18:44 by feedback          #+#    #+#             */
+/*   Updated: 2025/05/08 16:18:45 by feedback         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philos.h"
-ssize_t  tol(char *string)
+
+ssize_t	tol(char *string)
 {
 	int		i;
-	ssize_t res;
+	ssize_t	res;
 
 	res = 0;
 	i = 0;
@@ -10,60 +23,60 @@ ssize_t  tol(char *string)
 	{
 		printf("NO NEGATIVE NUMBERS PLEASE!\n");
 		printf("THE NUMBER YOU ENTERED IS INVALID IN MY PROJECT : %s", string);
-    return -1;
-  }
-  if (string[i] == '+')
+		return (-1);
+	}
+	if (string[i] == '+')
 		i++;
 	while (string[i] >= '0' && string[i] <= '9')
 	{
 		if (res > LIMIT)
 		{
 			printf("overflow !!! \n");
-      return -1;
-    }
-    res = res * 10 + (string[i] - '0');
+			return (-1);
+		}
+		res = res * 10 + (string[i] - '0');
 		i++;
 	}
-  if (string[i] != '\0')
-  {
-    printf("INVALID PARAM: %s\n", string);
-    return -1;
-  }
-  return (res);
+	if (string[i] != '\0')
+	{
+		printf("INVALID PARAM: %s\n", string);
+		return (-1);
+	}
+	return (res);
 }
 
-
-int starting(t_data *data , char **av  , int ac )
+int	starting(t_data *data, char **av, int ac)
 {
-
-  data->nop = tol(av[1]);
-  data->ttd = tol(av[2]);
-  data->tte = tol(av[3]);
-  data->tts = tol(av[4]);
-  if(ac == 6)
-    data->notme = tol(av[5]);
-  else
-    data->notme = -1;  
-  // data->start_time = time_now_ms();
-  if(data->nop == -1 || data->nop == 0  || data->tts == -1 || data->ttd == -1 || data->tte == -1 )
-  {
-    printf("be logical, try to ask the project owner what u should test if you don't know what you are doing\n");
-    return 1;
-  }
-  data->start_time = time_now_ms();
-  return 0;
+	data->nop = tol(av[1]);
+	data->ttd = tol(av[2]);
+	data->tte = tol(av[3]);
+	data->tts = tol(av[4]);
+	if (ac == 6)
+		data->notme = tol(av[5]);
+	else
+		data->notme = -1;
+	// data->start_time = time_now_ms();
+	if (data->nop == -1 || data->nop == 0 || data->tts == -1 || data->ttd == -1
+		|| data->tte == -1)
+	{
+		printf("be logical,
+			try to ask the project owner what u should test if you don't know what you are doing\n");
+		return (1);
+	}
+	data->start_time = time_now_ms();
+	return (0);
 }
 
-int main(int ac , char **av)
+int	main(int ac, char **av)
 {
-  t_data	data;
-  if(ac != 5 && ac != 6)
-  {
-    printf("TRY TO ENTER THE RIGHT ARGUMENTS HONEY :| ");
-    return 1;
-  }
-  if(starting(&data, av, ac))
-    return 1;
-  init(&data);
-  return (0);
+	t_data data;
+	if (ac != 5 && ac != 6)
+	{
+		printf("TRY TO ENTER THE RIGHT ARGUMENTS HONEY :| ");
+		return (1);
+	}
+	if (starting(&data, av, ac))
+		return (1);
+	init(&data);
+	return (0);
 }
